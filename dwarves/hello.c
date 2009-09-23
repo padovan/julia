00834659af65236571264b97bf4fc82208b54fa5
Luis Felipe Strano Moraes <lfelipe@profusion.mobi>
2009-09-23 14:56:49 -0300

    added example for pahole and slide showing how to use it.

---
+++ dwarves/hello.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+
+struct data1 {
+    char i;
+    double d;
+};
+
+struct data2 {
+    int i1;
+    double d;
+    int i2;
+};
+
+struct data3 {
+    int i1;
+    int i2;
+    double d;
+};
+
+int main()
+{
+    struct data1 a;
+    struct data2 b;
+    struct data3 c;
+	 printf("GPSL!!! \n");
+	 return 0;
+}
+
